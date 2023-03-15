1895b5cb08c6247158c262c94381464a4c6e29e0
andre_root <torkhovasic@yandex.ru>
2023-03-15 15:48:27 +0300

    'second version file'

--- app.py
+++ app.py
@@ -8,19 +8,47 @@ conn = psycopg2.connect(database="service_db", user="postgres", password="Aat891
 cursor = conn.cursor()
 
 
-@app.route('/login/', methods=['POST', 'GET'])
+@app.route('/login/', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        if request.form.get("login"):
+        if request.form.get('login'):
             username = request.form.get('username')
             password = request.form.get('password')
-            cursor.execute("SELECT * FROM service.users WHERE login=%s AND password=%s", (str(username), str(password)))
+
+            if not username or not password:
+                return render_template('login.html', message='Заполните обязательные поля')
+
+            cursor.execute('SELECT * FROM service.users WHERE login=%s AND password=%s', (str(username), str(password)))
             records = list(cursor.fetchall())
-            return render_template('account.html', full_name=records[0][1])
-        elif request.form.get("registration"):
-            return redirect("/registration/")
+
+            if not records:
+                return render_template('login.html', message='Такой комбинации логина и пароля не существует')
+
+            return render_template('account.html', full_name=records[0][1], login=login, password=password)
+        elif request.form.get('registration'):
+            return redirect('/registration')
     return render_template('login.html')
 
-@app.route('/login/', methods=['GET'])
-def index():
-    return render_template('login.html')
+
+@app.route('/registration/', methods=['POST', 'GET'])
+def registration():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        login = request.form.get('login')
+        password = request.form.get('password')
+
+        if not name or not login or not password:
+            return render_template('registration.html', message='Заполните все поля')
+
+        cursor.execute('SELECT * FROM service.users WHERE login=%s ', (str(login), ))
+        records = list(cursor.fetchall())
+        if records:
+            return render_template('registration.html', message='Пользователь с таким логином уже существует')
+
+        cursor.execute('INSERT INTO service.users (full_name, login, password) VALUES (%s, %s, %s);',
+                       (str(name), str(login), str(password)))
+
+        conn.commit()
+        return redirect('/login/')
+
+    return render_template('registration.html')
