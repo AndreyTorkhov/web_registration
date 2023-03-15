8ac180011ea4dca37c8633828f952f523fcb9144
andre_root <torkhovasic@yandex.ru>
2023-03-15 15:37:32 +0300

    'second version file'

--- app.py
+++ app.py
@@ -1,24 +1,25 @@
 import requests
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import psycopg2
+
 
 app = Flask(__name__)
 conn = psycopg2.connect(database="service_db", user="postgres", password="Aat8912000!", host="localhost", port="5432")
 cursor = conn.cursor()
 
-@app.route('/login/', methods=['POST'])
+
+@app.route('/login/', methods=['POST', 'GET'])
 def login():
-    username = request.form.get('username')
-    if username == '':
-        return render_template('login.html', error_msg1='enter username')
-    password = request.form.get('password')
-    if password == '':
-        return render_template('login.html', error_msg2='enter password')
-    cursor.execute("SELECT * FROM service.users WHERE login=%s AND password=%s", (str(username), str(password)))
-    records = list(cursor.fetchall())
-    if len(records) == 0:
-        return render_template('login.html', error_msg='user is not found')
-    return render_template('account.html', full_name=records[0][1], username=records[0][2], password=records[0][3])
+    if request.method == 'POST':
+        if request.form.get("login"):
+            username = request.form.get('username')
+            password = request.form.get('password')
+            cursor.execute("SELECT * FROM service.users WHERE login=%s AND password=%s", (str(username), str(password)))
+            records = list(cursor.fetchall())
+            return render_template('account.html', full_name=records[0][1])
+        elif request.form.get("registration"):
+            return redirect("/registration/")
+    return render_template('login.html')
 
 @app.route('/login/', methods=['GET'])
 def index():
